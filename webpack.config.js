6acfaaadbbab84ee12eafc3914914003e299ec0d
Yuriy <ykantakuzin@bk.ru>
2024-01-17 20:14:31 +0300

    Добавил public

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     initColorScheme: './src/features/colorScheme/initColorScheme.ts',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: '[name].[contenthash].js',
   },
   module: {
