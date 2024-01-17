b6d1926ca9a20cd09346c2ec1c0cde9d5731f55d
Yuriy <ykantakuzin@bk.ru>
2024-01-17 20:21:19 +0300

    исправил баг с подключением к api

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,16 +1,7 @@
 import { IRepository } from '@features/repositories/types';
 
-const githubApiUrl = 'https://api.github.com';
-const githubToken = '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';
-const requestHeaders = {
-  Accept: 'application/vnd.github+json',
-  Authorization: `Bearer ${githubToken}`,
-};
-
 export const apiFetchPopularRepositories = (): Promise<IRepository[]> => {
-  return fetch(`${githubApiUrl}/search/repositories?q=stars:>10000%20language:javascript&sort=stars`, {
-    headers: requestHeaders,
-  })
+  return fetch('https://api.github.com/search/repositories?q=JavaScript&sort=stars&per_page=30')
     .then((response) => response.json())
     .then((json) => json.items);
 };
